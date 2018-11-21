b8b5c4a5877315d22c38470f7d9ccaaf15276ca8
Govind Dixit <32304546+GOVINDDIXIT@users.noreply.github.com>
2018-11-21 14:00:51 +0530

    Add Image Picker
    Image Picker utility

---
+++ ImagePicker/app/src/main/java/app/dixit/govind/imagepicker/MainActivity.kt
@@ -0,0 +1,146 @@
+package app.dixit.govind.imagepicker
+
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.media.MediaScannerConnection
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var btn: Button? = null
+    private var imageview: ImageView? = null
+    private val GALLERY = 1
+    private val CAMERA = 2
+
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn = findViewById<View>(R.id.btn) as Button
+        imageview = findViewById<View>(R.id.iv) as ImageView
+
+        btn!!.setOnClickListener { showPictureDialog() }
+
+    }
+
+    private fun showPictureDialog() {
+        val pictureDialog = AlertDialog.Builder(this)
+        pictureDialog.setTitle("Select Action")
+        val pictureDialogItems = arrayOf("Select photo from gallery", "Capture photo from camera")
+        pictureDialog.setItems(pictureDialogItems
+        ) { dialog, which ->
+            when (which) {
+                0 -> choosePhotoFromGallary()
+                1 -> takePhotoFromCamera()
+            }
+        }
+        pictureDialog.show()
+    }
+
+    fun choosePhotoFromGallary() {
+        val galleryIntent = Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(galleryIntent, GALLERY)
+    }
+
+    private fun takePhotoFromCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA)
+    }
+
+    public override fun onActivityResult(requestCode:Int, resultCode:Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY)
+        {
+            if (data != null)
+            {
+                val contentURI = data!!.data
+                try
+                {
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                    val path = saveImage(bitmap)
+                    Toast.makeText(this@MainActivity, "Image Saved!", Toast.LENGTH_SHORT).show()
+                    imageview!!.setImageBitmap(bitmap)
+
+                }
+                catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(this@MainActivity, "Failed!", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
+        else if (requestCode == CAMERA)
+        {
+            if (resultCode == RESULT_CANCELED)
+            {
+                return
+            }
+            else {
+                val thumbnail = data!!.extras!!.get("data") as Bitmap
+                imageview!!.setImageBitmap(thumbnail)
+                saveImage(thumbnail)
+                Toast.makeText(this@MainActivity, "Image Saved!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun saveImage(myBitmap: Bitmap):String {
+        val bytes = ByteArrayOutputStream()
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes)
+        val wallpaperDirectory = File(
+                (Environment.getExternalStorageDirectory()).toString() + IMAGE_DIRECTORY)
+        // have the object build the directory structure, if needed.
+        Log.d("fee",wallpaperDirectory.toString())
+        if (!wallpaperDirectory.exists())
+        {
+
+            wallpaperDirectory.mkdirs()
+        }
+
+        try
+        {
+            Log.d("heel",wallpaperDirectory.toString())
+            val f = File(wallpaperDirectory, ((Calendar.getInstance()
+                    .getTimeInMillis()).toString() + ".jpg"))
+            f.createNewFile()
+            val fo = FileOutputStream(f)
+            fo.write(bytes.toByteArray())
+            MediaScannerConnection.scanFile(this,
+                    arrayOf(f.getPath()),
+                    arrayOf("image/jpeg"), null)
+            fo.close()
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath())
+
+            return f.getAbsolutePath()
+        }
+        catch (e1: IOException) {
+            e1.printStackTrace()
+        }
+
+        return ""
+    }
+
+    companion object {
+        private val IMAGE_DIRECTORY = "/demonuts"
+    }
+
+}
